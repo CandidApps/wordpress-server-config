1e252de22955345b7097744688194f1880f267fe
Aahan Krish <krish@aahan.me>
2013-09-21 22:55:28 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -2,8 +2,7 @@
 
 # Tell WordPress/PHP to get client IP address from
 # X-Forwarded-For HTTP header set by Varnish
-#
-# SOURCE: http://ocaoimh.ie/2011/08/09/speed-up-wordpress-with-apache-and-varnish/
+# http://ocaoimh.ie/2011/08/09/speed-up-wordpress-with-apache-and-varnish/
 if ( isset( $_SERVER[ "HTTP_X_FORWARDED_FOR" ] ) ) {
 	$_SERVER[ 'REMOTE_ADDR' ] = $_SERVER[ "HTTP_X_FORWARDED_FOR" ];
 }
