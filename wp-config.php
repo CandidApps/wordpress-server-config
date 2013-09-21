b5be2911b3905b509b09a8d047b467ebc2faef79
Aahan Krish <krish@aahan.me>
2013-09-21 22:56:50 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -5,3 +5,15 @@
 if ( isset( $_SERVER[ "HTTP_X_FORWARDED_FOR" ] ) ) {
 	$_SERVER[ 'REMOTE_ADDR' ] = $_SERVER[ "HTTP_X_FORWARDED_FOR" ];
 }
+
+	# Set cookie domain
+	define( 'COOKIE_DOMAIN', '.example.com' );
+
+	# Disable post revisions
+	define( 'WP_POST_REVISIONS', false );
+
+	# Set autosave interval
+	# define( 'AUTOSAVE_INTERVAL', 160 );
+
+	# Increase PHP memory limit
+	# define( 'WP_MEMORY_LIMIT', '64M' );
