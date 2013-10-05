1a25ed39baa36bb27f970b1c26c1648b19b14501
Aahan Krish <krish@aahan.me>
2013-10-05 21:44:11 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,10 +1,19 @@
 <?php
 
+/*
+ * If using Varnish
+ */
+
 # Tell WordPress/PHP to get client IP address from X-Forwarded-For HTTP header set by Varnish
 # http://ocaoimh.ie/2011/08/09/speed-up-wordpress-with-apache-and-varnish/
 if ( isset( $_SERVER[ "HTTP_X_FORWARDED_FOR" ] ) ) {
 	$_SERVER[ 'REMOTE_ADDR' ] = $_SERVER[ "HTTP_X_FORWARDED_FOR" ];
 }
+
+
+/*
+ * General Configurations
+ */
 
 # Set cookie domain so that cookies aren't sent with static content
 # Only works if static content is served from a different domain
@@ -18,6 +27,11 @@ define( 'WP_POST_REVISIONS', false );
 
 # Increase PHP memory limit
 # define( 'WP_MEMORY_LIMIT', '64M' );
+
+
+/*
+ * Multisite-specific Configurations
+ */
 
 # Enable Multisite
 # define( 'WP_ALLOW_MULTISITE', true );
